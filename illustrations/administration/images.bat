88c6c0b497d442a0903f7de99e6681c3adee7f84
Kristian Hennings <krhe@inventors.dk>
2025-09-20 12:25:43 +0200

    Added images for the LabBench Admin guide

---
+++ illustrations/administration/images.bat
@@ -0,0 +1,29 @@
+cd illustrations
+
+magick Slide1.PNG -trim LabBenchDesigner.png 
+magick Slide2.PNG -trim SystemLogging.png 
+magick Slide3.PNG -trim SystemLicense.png 
+magick Slide4.PNG -trim DialogLicenseEnterKey.png 
+magick Slide5.PNG -trim DialogLicenseRegister.png 
+magick Slide6.PNG -trim SystemLicenseDeregister.png 
+magick Slide7.PNG -trim DevicesPage.png 
+magick Slide8.PNG -trim DialogAddDevices.png 
+magick Slide9.PNG -trim DialogAddDevicesBluetoothProblem.png 
+magick Slide10.PNG -trim ProtocolsPage.png 
+magick Slide11.PNG -trim BluetoothSerialPorts.png 
+magick Slide12.PNG -trim ExperimentsPage.png 
+magick Slide13.PNG -trim ExperimentsSubjectID.png 
+magick Slide14.PNG -trim DialogSubjectID.png 
+magick Slide15.PNG -trim ExperimentsSetup.png 
+magick Slide16.PNG -trim DialogConfigureDevices.png 
+magick Slide17.PNG -trim DialogConfigureJoystick.png 
+magick Slide18.PNG -trim DialogConfigureDisplay.png 
+magick Slide19.PNG -trim ExperimentsActions.png 
+magick Slide20.PNG -trim ExperimentsData.png 
+magick Slide21.PNG -trim DialogConfigureRepositories.png 
+magick Slide22.PNG -trim DialogCreateExperiment.png 
+
+del Slide*.PNG
+
+cd ..
+pause
